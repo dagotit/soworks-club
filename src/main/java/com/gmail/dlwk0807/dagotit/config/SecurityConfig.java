ed9f9ea311edfcb0012c879da8480dd39c93628e
churihyung <dlwk0807@gmail.com>
2023-11-07 10:50:58 +0900

    cors 설정 추가

--- src/main/java/com/gmail/dlwk0807/dagotit/config/SecurityConfig.java
+++ src/main/java/com/gmail/dlwk0807/dagotit/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,6 +20,7 @@ public class SecurityConfig {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,6 +32,7 @@ public class SecurityConfig {
             // CSRF 설정 Disable
         http
             .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .exceptionHandling((exceptionHandling) ->
                 exceptionHandling
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
--- src/main/java/com/gmail/dlwk0807/dagotit/controller/AuthController.java
+++ src/main/java/com/gmail/dlwk0807/dagotit/controller/AuthController.java
@@ -33,3 +33,4 @@ public class AuthController {
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
     }
 }
+
