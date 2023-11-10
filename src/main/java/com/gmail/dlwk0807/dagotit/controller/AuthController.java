aaaf65a0342a4441698607701a2b39b819783ec4
churihyung <dlwk0807@gmail.com>
2023-11-10 17:33:41 +0900

    refresh token set cookie

--- src/main/java/com/gmail/dlwk0807/dagotit/controller/AuthController.java
+++ src/main/java/com/gmail/dlwk0807/dagotit/controller/AuthController.java
@@ -5,7 +5,9 @@ import com.gmail.dlwk0807.dagotit.controller.dto.MemberResponseDto;
 import com.gmail.dlwk0807.dagotit.controller.dto.TokenDto;
 import com.gmail.dlwk0807.dagotit.controller.dto.TokenRequestDto;
 import com.gmail.dlwk0807.dagotit.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,13 +26,36 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
-        return ResponseEntity.ok(authService.login(memberRequestDto));
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto, HttpServletResponse response) {
+
+        TokenDto token = authService.login(memberRequestDto);
+
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", token.getRefreshToken())
+                .maxAge(7 * 24 * 60 * 60)
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .build();
+        response.setHeader("Set-Cookie", cookie.toString());
+
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/reissue")
-    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
-        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto, HttpServletResponse response) {
+        TokenDto reissue = authService.reissue(tokenRequestDto);
+
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", reissue.getRefreshToken())
+                .maxAge(7 * 24 * 60 * 60)
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .build();
+        response.setHeader("Set-Cookie", cookie.toString());
+
+        return ResponseEntity.ok(reissue);
     }
 }
 
