d96a66eea1dcd9a21ffad0de388c3ed11e255bc8
정승호 (주)소프트웍스/임시 <dkdl1566@softworks.co.kr>
2023-11-20 17:27:26 +0900

    #11 Feat: 회원가입(depth 1)

---
+++ front/src/components/popups/AddressSearch.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
+
+const AddressSearch = ({ onSelectAddress }) => {
+  const scriptUrl= 'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+  const open = useDaumPostcodePopup(scriptUrl);
+  const [selectedZipCode, setSelectedZipCode] = useState('');
+  const [selectedAddress, setSelectedAddress] = useState('');
+
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let zipCode = data.zonecode;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+    setSelectedZipCode(zipCode); // 검색한 우편 번호 저장
+    setSelectedAddress(fullAddress); // 검색한 주소 저장
+    onSelectAddress({ selectedAddress: fullAddress, selectedZipCode: zipCode });
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
+  return (
+    <button type='button' onClick={handleClick}>
+      우편번호 검색
+    </button>
+  );
+};
+
+export default AddressSearch;
