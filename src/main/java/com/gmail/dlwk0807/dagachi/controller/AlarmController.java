89394143e6f562ec990934386e6b027c9014909a
churihyung <dlwk0807@gmail.com>
2024-04-18 15:31:17 +0900

    1. build clean 2. alarm swagger 등록

--- src/main/java/com/gmail/dlwk0807/dagachi/controller/AlarmController.java
+++ src/main/java/com/gmail/dlwk0807/dagachi/controller/AlarmController.java
@@ -3,6 +3,11 @@ package com.gmail.dlwk0807.dagachi.controller;
 import com.gmail.dlwk0807.dagachi.service.AlarmService;
 import com.gmail.dlwk0807.dagachi.vo.ApiMessageVO;
 import com.google.firebase.messaging.FirebaseMessaging;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +21,7 @@ import static com.gmail.dlwk0807.dagachi.util.SecurityUtils.getCurrentMemberId;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/alarm")
+@Tag(name = "ALARM API", description = "알람 API")
 public class AlarmController {
 
     private final FirebaseMessaging firebaseMessaging;
@@ -35,6 +41,12 @@ public class AlarmController {
 //        firebaseMessaging.send(fcmMessage);
 //    }
 
+    @Operation(summary = "알람리스트")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "Error Code",description = "Error message",
+                    content = @Content(schema = @Schema(implementation = ApiMessageVO.class))),
+    })
     @GetMapping("/list")
     public ApiMessageVO listAlarm() {
 
@@ -45,6 +57,12 @@ public class AlarmController {
                 .build();
     }
 
+    @Operation(summary = "알람ID로 찾기")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "Error Code",description = "Error message",
+                    content = @Content(schema = @Schema(implementation = ApiMessageVO.class))),
+    })
     @GetMapping("/{id}")
     public ApiMessageVO findAlarm(@PathVariable Long id) {
 
