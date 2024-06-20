4467aa39dd898c7e068c0bbbcd961c70cb6cc349
정승호 (주)소프트웍스/임시 <dkdl1566@softworks.co.kr>
2024-06-20 14:55:09 +0900

    #93 Feat: 메인페이지 퍼블수정

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,11 @@
 import { Fragment } from 'react';
 import Main from '@/app/(main)/Main';
-import Bg from '@/components/bgBox/Bg';
 import Header from '@/components/Header';
+import '@/app/globals.css';
 
 export default function Home() {
   return (
     <Fragment>
-      <Bg />
       <Main />
     </Fragment>
   )
